36a62ea9ae5e6e3b48c830e286b5f0f3eb88757d
Trevor West <trevorallenwest@gmail.com>
2021-06-17 18:34:18 +0900

    Add rock-paper-scissors HTML and JS files

---
+++ rock-paper-scissors.js
@@ -0,0 +1,87 @@
+/* BEGIN 1. UNDERSTANDING (Problem Outline and Strategy)
+
+Welcome to Rock Paper Scissors, The Odin Project game! 
+ The point of this program is to simulate the classic rock,
+ paper, scissors game that most played as kids.
+
+ I will be following the 'Programmer Mindset' philosophy
+ to problem solving which involves doing 3 main things
+ when approaching a problem (and specifically a coding
+ problem):
+
+ First: Understand the problem. (I am doing this here in
+    this larger introductory comment section);
+
+ Second: Plan (I will do this by outlining more specific
+    comments of everything the code needs to do, based
+    on my understanding as outlined in this introductory
+    comment section)
+
+ Third: Break down (I will do this by translating the parts
+    of comments to code that are easiest)
+
+ So far, in learning more about coding, and having a
+ background in Japanese-English translation, I have come
+ to view programming to be less a matter of "speaking" a
+ foreign language so much as translating between human
+ logic and computer logic; from human language and desire
+ to computer language and execution. So to "solve" a coding
+ problem seems to me to require an adequate "translation."
+
+ As for the game of rock-paper-scissors itself:
+ 
+ The basic rules are simple:
+ 1. Rock beats scissors
+ 2. Scissor beats paper
+ 3. Paper beats rock.
+ 
+ The extent of this project is to create variables to store
+ the inputs from both the player who will be inputting his
+ or her choice of rock, scissor, or paper; he or she will
+ be playing against a computer (function) that will 
+ randomly generate a numerical value that will be assigned
+ rock, scissor, or paper in a random 1/3 chance ratio.
+ 
+ Upon the player beating the computer, a simple "Yay!
+ You won~!" message will be printed, and upon losing a
+ simple "Oh no, the computer won :(" message will be
+ printed.
+ 
+ That sums up the basic understanding of the problem,
+ next will be for outlining each step of the code in
+ comments as part of phase 2: Planning.*/
+
+
+ /* BEGIN 2. PLAN (Comment outlining of everything needed)*/
+
+ // Create a variable to store player's choice
+let playerSelection;
+
+ // Create a variable to store computer's choice
+ let computerChoice;
+
+
+ // Create function computerPlay and use the stored random
+ // value to determine rock, paper, or scissors.
+ // Ensure that this function is repeated at the end/start
+ // of each new round so that the computer always has a 
+ // different value (so game doesn't suck/isn't predictable every time)
+
+
+ // Create a function to randomly generate a random value
+ 
+
+ // Create variables to associate the same numerial value
+ // ranges with rock, paper, scissors (in such a way as to equal 1/3)
+
+
+ // Create an if loop to determine if player of computer wins
+
+ // Print out a "Yay~! You won!" message (part of loop)
+
+ // Print out a "Oh no! You lost to the computer :(" message
+ // (part if loop)
+
+
+
+
