3d055fd30622f75173a243cfe5ec920ea0175d28
Trevor West <indestructiblelion@tutanota.com>
2021-10-27 11:49:07 -0500

    initial commit to rps-ui branch, update rock-paper-scissors.html to index.html, update rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,6 @@
 /* BEGIN 1. UNDERSTANDING (Problem Outline and Strategy)
 
-Welcome to Rock Paper Scissors, The Odin Project game! 
+Welcome to Rock Paper Scissors, The Odin Project game!
  The point of this program is to simulate the classic rock,
  paper, scissors game that most played as kids.
 
@@ -29,24 +29,24 @@ Welcome to Rock Paper Scissors, The Odin Project game!
  problem seems to me to require an adequate "translation."
 
  As for the game of rock-paper-scissors itself:
- 
+
  The basic rules are simple:
  1. Rock beats scissors
  2. Scissor beats paper
  3. Paper beats rock.
- 
+
  The extent of this project is to create variables to store
  the inputs from both the player who will be inputting his
  or her choice of rock, scissor, or paper; he or she will
- be playing against a computer (function) that will 
+ be playing against a computer (function) that will
  randomly generate a numerical value that will be assigned
  rock, scissor, or paper in a random 1/3 chance ratio.
- 
+
  Upon the player beating the computer, a simple "Yay!
  You won~!" message will be printed, and upon losing a
  simple "Oh no, the computer won :(" message will be
  printed.
- 
+
  That sums up the basic understanding of the problem,
  next will be for outlining each step of the code in
  comments as part of phase 2: Planning.*/
@@ -58,7 +58,7 @@ Welcome to Rock Paper Scissors, The Odin Project game!
  // Create function computerPlay and use the stored random
  // value to determine rock, paper, or scissors.
  // Ensure that this function is repeated at the end/start
- // of each new round so that the computer always has a 
+ // of each new round so that the computer always has a
  // different value (so game doesn't suck/isn't predictable every time)
  function computerPlay() {
     const gameArray = ['Rock', 'Paper', 'Scissors'];
@@ -72,7 +72,7 @@ Welcome to Rock Paper Scissors, The Odin Project game!
  // Create a variable/subfunction that removes the need to have case
  // sensitive typing of rock, paper, or scissors (i.e., a function
  // that will take the player's input and turn it into all lowercase
- // and that is what the main function will be checking against 
+ // and that is what the main function will be checking against
  // for the true/false statements)
  // Ensure that this function is repeated at the end/start
  // of each new round
@@ -88,8 +88,8 @@ function playerPlay() {
    else if (playerChoice.toLowerCase() === "scissors" || parseInt(playerChoice) === 3) {
       playerChoice = "Scissors";
    }
-   else if (playerChoice.toLowerCase() !== "rock" || playerChoice.toLowerCase() !== "scissors" || 
-         playerChoice.toLowerCase() !== "paper" || parseInt(playerChoice) !== 1 || 
+   else if (playerChoice.toLowerCase() !== "rock" || playerChoice.toLowerCase() !== "scissors" ||
+         playerChoice.toLowerCase() !== "paper" || parseInt(playerChoice) !== 1 ||
          parseInt(playerChoice) !== 2 || parseInt(playerChoice) !== 3) {
       alert("Please try to enter your value again :)")
       playerSelection();
@@ -101,7 +101,7 @@ function playerPlay() {
 }
 
 
-/* Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and 
+/* Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and
 computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock" */
 
 function playRound(playerSelection, computerSelection) {
@@ -115,37 +115,37 @@ function playRound(playerSelection, computerSelection) {
       console.log("Rock beats scissors! You lose :(");
       playerScore;
       computerScore = computerScore + 1;
-      return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection 
+      return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection
       + ". " + "Your Score: " + playerScore + ". " + "Computer Score: " + computerScore);
    } else if (computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "rock") {
       console.log("Rock beats scissors! You win :D");
       playerScore = playerScore + 1;
       computerScore;
-      return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection 
+      return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection
       + ". " + "Your Score: " + playerScore + ". " + "Computer Score: " + computerScore);
    } else if (computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "paper") {
       console.log("Scissors beats paper! You lose :(");
       playerScore;
       computerScore = computerScore + 1;
-      return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection 
+      return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection
       + ". " + "Your Score: " + playerScore + ". " + "Computer Score: " + computerScore);
    } else if (computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "scissors") {
       console.log("Scissors beats paper! You win :D");
       playerScore = playerScore + 1;
       computerScore;
-      return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection 
+      return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection
       + ". " + "Your Score: " + playerScore + ". " + "Computer Score: " + computerScore);
    } else if (computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "rock") {
       console.log("Paper beats rock! You lose :(");
       playerScore;
       computerScore = computerScore + 1;
-      return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection 
+      return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection
       + ". " + "Your Score: " + playerScore + ". " + "Computer Score: " + computerScore);
    } else if (computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "paper") {
       console.log("Paper beats rock! You win :D");
       playerScore = playerScore + 1;
       computerScore;
-      return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection 
+      return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection
       + ". " + "Your Score: " + playerScore + ". " + "Computer Score: " + computerScore);
    } else {
       console.log("I'm not sure what, but something went wrong :(");
@@ -160,7 +160,7 @@ let computerScore = 0;
 // Define function / rule for winning and keeping track of score and determining winner for 5 rounds of game
 // Make each round reset the computer choice and get new input from the player.
 
-
+/*
 function game() {
  for (let i = 0; i < 5; i++) {
     const playerSelection = playerPlay();
@@ -168,6 +168,44 @@ function game() {
     const currentRound = playRound(playerSelection, computerSelection);
     console.log(currentRound);
  }
+*/
+
+let playerSelection = "";
+let computerSelection = "";
+
+// define rock and listens for rock button click then saves rock value to playerSelection
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+  playerSelection = "rock";
+  console.log(playerSelection);
+});
+
+// define rock and listens for rock button click then saves rock value to playerSelection
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+  playerSelection = "paper";
+  console.log(playerSelection);
+});
+
+// define rock and listens for rock button click then saves rock value to playerSelection
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+  playerSelection = "scissors";
+  console.log(playerSelection);
+});
+
+
+/*
+document.getElementById('paper').addEventListener('click', function(e){
+
+});
+document.getElementById('scissors').addEventListener('click', function(e){
+
+});
+*/
+
+
+
 // Prints win or lose message based on final score after running 5 rounds of game
  if (playerScore > computerScore) {
    console.log("Congratulations! You won the game! :)");
@@ -181,4 +219,3 @@ function game() {
 // Reset playerScore and computerScore back to zero at end of 5 rounds / one game set
 playerScore = 0;
 computerScore = 0;
-}
